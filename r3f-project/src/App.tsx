9f096d61369f5f27ce236b81c30926e729bd8801
Tim Coleman || ThunderFarm <tim@thunder.love>
2026-02-11 18:48:43 -0500

    updated imports for Vercel

--- r3f-project/src/App.tsx
+++ r3f-project/src/App.tsx
@@ -6,23 +6,15 @@ import { Bldg22Model } from "./components/Bldg22Model";
 import { CameraController } from "./controllers/CameraController"
 import { CAMERA_POSITIONS, getBayCamera } from "./controllers/cameraPositions";
 import type { ViewMode, Selection } from "./types/viewTypes";
-import './index.css'
-import './App.css' 
-
 import rawInventory from "./data/inventory.json";
-import type { Inventory } from "./types/Inventory";
-
-
-
-import type { InventoryApi } from "./types/InventoryAPI"; // note: InventoryAPI.ts file
-
-
+import type { Inventory } from "../src/types/Inventory";
+import type { InventoryApi } from "../src/types/InventoryAPI"; // note: InventoryAPI.ts file
 import { mapInventory } from "./utils/mapInventory";
 import { InventoryDropdown } from "./components/InventoryDropdown";
-
 import companyLogo from "../src/assets/ccsoft_logo_opt.png"
 import { Compass } from "./components/Compass";
-
+import './index.css'
+import './App.css' 
 
 
 
